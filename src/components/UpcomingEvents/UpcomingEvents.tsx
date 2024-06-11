d9707ed25077e604dde2554b8b057659f73e9507
Andreas Papazafeiropoulos <andreaspapaz@gmail.com>
2024-06-11 09:22:54 -0500

    events

--- src/components/UpcomingEvents/UpcomingEvents.tsx
+++ src/components/UpcomingEvents/UpcomingEvents.tsx
@@ -3,20 +3,38 @@ import Event from "./Event";
 const UpcomingEvents = () => {
   const eventData = [
     {
-      month: "Jan",
+      month: "Sept",
       date: "27",
-      title: "DASTAAN - a tribute to Rekha",
+      title: "$_GRAND_RAPIDS - 62 6f 6f 74 69 6e 67 2e 2e 2e",
       image:
-        "https://images.unsplash.com/photo-1475721027785-f74eccf877e2?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTJ8fGV2ZW50fGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=500&q=60",
+        "https://grrcon.com/wp-content/uploads/2022/10/Grr2023-828x1024.png.webp",
       venue: {
-        name: "Gyan Manch",
-        directions: "https://www.google.co.in",
+        name: "DeVos Place",
+        directions:
+          "https://www.google.com/maps?gs_lcrp=EgZjaHJvbWUyBggAEEUYOTIGCAEQLhhA0gEHMTEzajBqMagCALACAA&um=1&ie=UTF-8&fb=1&gl=us&sa=X&geocode=Ke2_cMbPrRmIMSmkgPqNOrO4&daddr=303+Monroe+Ave+NW,+Grand+Rapids,+MI+49503",
       },
-      time: "Saturday, Jan 27, 2018 at 5:30 PM",
-      timestamp: "1517054400",
+      time: "September 26 & 27, 2024",
+      timestamp: "nan",
       story:
-        '"For a woman to be complete, she has to be a blend of Paro & Chandramukhi. I feel that I am that woman." - Rekha',
-      ticketLink: "https://www.google.com",
+        "GrrCON is an information security and hacking conference that provide the Information Security community with a fun atmosphere to come together and engage with likeminded people from around the globe. We keep GrrCON small with around 2,500 attendance to allow us to provide those things other events leave out. Whether you are a Fortune 500 executive, security researcher, industry professional, student, or hacker you will find something for you at GrrCON.",
+      ticketLink: "https://grrcon.com/",
+    },
+    {
+      month: "June",
+      date: "8",
+      title: "$_CHICAGO - 62 6f 6f 74 69 6e 67 2e 2e 2e",
+      image: "https://www.hak4kidz.com/images/computerhacked.png",
+      venue: {
+        name: "Cisco Systems - 433 W. Van Buren St., 7th floor, Chicago, IL",
+        directions:
+          "https://www.google.com/maps/dir//433+W+Van+Buren+St+7th+floor,+Chicago,+IL+60607/@41.8765027,-87.6386911,17z/data=!4m9!4m8!1m0!1m5!1m1!1s0x880e2d5a8457cca3:0xa88b5fafa6697eaf!2m2!1d-87.6386911!2d41.8765027!3e0?entry=ttu",
+      },
+      time: "Saturday June 8th, 2024, 8:30am",
+      timestamp: "1717843913",
+      story:
+        "There are a lot of parking options near the venue with SpotHero and ParkChirp or another app you enjoy. We are about 1 block south from Union Station, if you prefer that mode of transportation.  ** Bring A Laptop For Your Kid(z) **",
+      ticketLink:
+        "https://www.eventbrite.com/e/hak4kidz-2024-tickets-816779127357?aff=home",
     },
   ];
 
